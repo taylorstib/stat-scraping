1ba355a2a9c746fef24fb61ca64394e894c09890
Taylor Stib L <tstib3@gmail.com>
2015-01-30 11:18:43 -0700

    Set up first failing tests

--- spec/espn_page_grab_spec.rb
+++ spec/espn_page_grab_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper.rb'
 
-Rspec.describe Scraper
+describe Scraper do
     describe '#scrape' do
-      it 'Grabs a page' do
-    end
+      it 'Finds the right ESPN page' do
+        expect(Scraper.new.scrape('passing')).to output.to_stderr
+      end
+
+      it 'Grabs the corresponding pages'
   end
 end
