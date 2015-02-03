d68a16f5513b8dee532f5e9b2fcf98de4cded1d5
Taylor Stib L <tstib3@gmail.com>
2015-02-03 11:05:01 -0700

    Complete the get_html method for Scraper

--- lib/espn_page_grab.rb
+++ lib/espn_page_grab.rb
@@ -17,35 +17,35 @@ class Scraper
     
     page = Nokogiri::HTML(open(BASE_URL + BASE_DIR[position.to_sym] + '1'))
     ## Debugging
-    STDERR.puts "Found '#{page.title}'"
-    #### Writes the first file ####
+    # STDERR.puts "Found '#{page.title}'"
+    # #### Writes the first file ####
     # File.open("espn_html_pages/#{position}1.html", 'w'){|f| f.write(page.to_html)}
     # STDERR.puts "First #{position} file saved successfully"
 
-    # #### Find the number of results(players) for the given stat request ####
-    # results_div = page.css('#my-players-table div.totalResults').first.content
-    # number_results_arr = results_div.split(' ')
-    # number_results = number_results_arr[0].to_i
+    #### Find the number of results(players) for the given stat request ####
+    results_div = page.css('#my-players-table div.totalResults').first.content
+    number_results_arr = results_div.split(' ')
+    number_results = number_results_arr[0].to_i
 
-    # #### Finding the correct number of pages to find ####
-    # if number_results % 40 == 0
-    #   num_pages = number_results/40
-    # else
-    #   num_pages = (number_results/40) + 1
-    # end
+    #### Finding the correct number of pages to find ####
+    if number_results % 40 == 0
+      num_pages = number_results/40
+    else
+      num_pages = (number_results/40) + 1
+    end
 
-    # STDERR.puts "#{num_pages - 1} more pages need to be collected"
+    STDERR.puts "#{num_pages - 1} more pages need to be collected"
 
-    # if num_pages > 1
-    #   #### Grabs each page of stats for passing ####
-    #   arr[(1..(num_pages-1))].each do |x|
-    #       page = Nokogiri::HTML(open(BASE_URL + BASE_DIR[position.to_sym] + "#{x}"))
-    #       #### Writes page to an HTML file on disk ####
-    #       File.open("espn_html_pages/#{position}#{x}.html", 'w'){|f| f.write(page.to_html)}
-    #       STDERR.puts "File #{position}#{x} saved successfully"
-    #   end
-    # end
-    # STDERR.puts "Reached the end of the 'scrape #{position}' method"
+    if num_pages > 1
+      #### Grabs each page of stats for passing ####
+      arr[(1..(num_pages-1))].each do |x|
+          page = Nokogiri::HTML(open(BASE_URL + BASE_DIR[position.to_sym] + "#{x}"))
+          #### Writes page to an HTML file on disk ####
+          File.open("espn_html_pages/#{position}#{x}.html", 'w'){|f| f.write(page.to_html)}
+          STDERR.puts "File #{position}#{x} saved successfully"
+      end
+    end
+    STDERR.puts "Reached the end of the 'scrape #{position}' method"
 
   end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,18 +3,22 @@ require 'nokogiri'
 require 'open-uri'
 require 'fileutils'
 
-
+# This URL defines the 'trunk' of all the NFL stat pages. 
 BASE_URL = 'http://espn.go.com/nfl/statistics/player/_/stat/'
-BASE_DIR = {:passing => 'passing/sort/passingYards/year/2013/seasontype/2/qualified/false/count/',
-            :rushing => 'rushing/sort/rushingYards/year/2013/seasontype/2/qualified/false/count/',
-            :receiving => 'receiving/sort/receivingYards/year/2013/seasontype/2/qualified/false/count/',
+# Each position has a slightly different URL structure and they are bound to change since this is not
+# a robust API
+BASE_DIR = { passing: 'passing/sort/passingYards/year/2013/seasontype/2/qualified/false/count/',
+             rushing: 'rushing/sort/rushingYards/year/2013/seasontype/2/qualified/false/count/',
+             receiving: 'receiving/sort/receivingYards/year/2013/seasontype/2/qualified/false/count/'
           }
 
 # This class scrapes ESPN using Nokogiri for further parsing
 # of the stats 
 class Scraper
-  attr_accessor :position, :arr, :page
+  attr_accessor :position, :array_of_count, :pages, :page_quantity
 
+# Initialize the Scraper class with the position you want stats for. Options
+# include 'passing', 'rushing', and 'receiving'.
   def initialize(position)
     if BASE_DIR.keys.include?(position.to_sym)
       @position = position
@@ -22,15 +26,43 @@ class Scraper
       raise RuntimeError, "Must be appropriate position (passing, rushing or receiving)"
     end
     @array_of_count = []
-    @page = ''
+    @pages = []
   end
 
+# Generates an array of numbers, counting by how many player records
+# are on each HTML page. Allows other methods to paginate through the 
+# stat pages and collect them all.
   def generate_count
     counts = (1..921).step(40) { |i| @array_of_count.push(i) }
     @array_of_count
   end
 
+# Access the URL and Nokogiri grabs the HTML, parsing it to determine how many pages need
+# to be scraped for the given position
   def get_html_page
-    @page = Nokogiri::HTML(open(BASE_URL + BASE_DIR[position.to_sym] + '1'))
+    @pages.push(Nokogiri::HTML(open(BASE_URL + BASE_DIR[position.to_sym] + '1')))
+    
+    results_div = @pages[0].css('#my-players-table div.totalResults').first.content
+    number_results_arr = results_div.split(' ')
+    number_results = number_results_arr[0].to_i
+
+    if number_results % 40 == 0
+      @page_quantity = number_results/40
+    else
+      @page_quantity = (number_results/40) + 1
+    end
+
+    if @page_quantity > 1
+      @array_of_count[1..(@page_quantity - 1)].each do |n|
+        STDERR.puts "Finding page #{n + 1}"
+        @pages.push(Nokogiri::HTML(open(BASE_URL + BASE_DIR[position.to_sym] + "#{n}")))
+        STDERR.puts "Page #{n + 1} added\n"
+        sleep 2  ## reduce load on server
+      end
+    end
+  end
+
+  def write_to_file
+
   end
 end
