298378ad5eee866d59e6392b4af20445a8585014
Taylor Stib L <tstib3@gmail.com>
2015-02-19 22:03:48 -0700

    Add code to run the Parser

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require_relative './lib/scraper.rb'
 require_relative './lib/parser.rb'
 
-# years = (2002..2014)
+years = (2005..2014)
 # years.each_with_index do |yr, i|
 #   rush = Scraper.new('rushing', yr)
 #   pass = Scraper.new('passing', yr)
@@ -17,13 +17,18 @@ require_relative './lib/parser.rb'
 # end
   
 
-# rush = Scraper.new('rushing', '2014')
-# puts rush.array_of_count
-par = Parser.new
-par.open_pages
-par.row_length
-par.write_to_csv
-blah = Parser.new('receiving')
-blah.open_pages
-blah.row_length
-blah.write_to_csv
+years.each_with_index do |yr, i|
+  rush = Parser.new('rushing', yr)
+  pass = Parser.new('passing', yr)
+  rec = Parser.new('receiving', yr)
+## Run methods once each instance has been instantiated
+  rush.open_pages
+  rush.row_length
+  rush.write_to_csv
+  pass.open_pages
+  pass.row_length
+  pass.write_to_csv
+  rec.open_pages
+  rec.row_length
+  rec.write_to_csv
+end
